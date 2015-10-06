a308a4b7ab11a73fbb6ea55760b292c57f1bb811
vashishta <adi.vashisht@agilex.com>
2015-10-06 17:08:48 -0400

    Spring MVC Project

---
+++ src/main/java/com/indasil/springmvc/web/controller/LandingController.java
@@ -0,0 +1,25 @@
+package com.indasil.springmvc.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Created by vashishta on 10/6/15.
+ */
+@Controller
+@RequestMapping("/app")
+public class LandingController {
+
+
+    @RequestMapping(value="/landing",method = RequestMethod.GET)
+    public String get() {
+        return "landing";
+    }
+
+    @RequestMapping(value="/pageTwo",method = RequestMethod.GET)
+    public String two() {
+        return "two";
+    }
+
+}
